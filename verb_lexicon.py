b5605f05d573cb037cad93ee298cd2d0d18ec238
rukiyeaslan <19rasln@gmail.com>
2023-12-30 15:31:14 +0300

    add verb lexicon

---
+++ verb_lexicon.py
@@ -0,0 +1,18 @@
+verb_lexicon = [
+    "aç", "ağla", "ak", "al", "anla", "anlat", "ara", "art", "aş", "atıl", "at", "ayır", "ayrıl", "azal", "bağır", 
+    "bağla", "bak", "bas", "başla", "bekle", "belirle", "belirlen", "belirt", "benz", "bırak", "bilin", "bil", 
+    "bin", "bitir", "bit", "bozul", "bul", "bulun", "büyü", "çalış", "çal", "çekil", "çek", "çevir", "çıkarıl", 
+    "çıkar", "çık", "çiz", "dayan", "değerlendir", "değiş", "değiştir", "de", "dem", "devam et", "dikkat et", 
+    "dile", "dinle", "doğ", "dolaş", "doldur", "dön", "dönüş", "dur", "duy", "düş", "düşün", "düzenle", "ekle", 
+    "etkile", "et", "evlen", "fark et", "geçir", "geç", "geliş", "geliştir", "gel", "gerçekleş", "gere", "getir", 
+    "gir", "git", "giy", "gönder", "gör", "görün", "görüş", "göster", "götür", "gül", "hareket et", "hatırla", 
+    "hazırla", "hazırlan", "hisset", "iç", "ifade et", "ilerle", "ilgilen", "inan", "incele", "in", "istem", "isten", 
+    "izle", "kabul et", "kaç", "kaldır", "kalk", "kal", "kapan", "kapat", "karış", "karıştır", "karşıla", "karşılaş", 
+    "katıl", "kaybet", "kazan", "kesil", "kes", "kıl", "konuş", "kork", "koru", "koş", "koy", "kullanıl", "kullan", 
+    "kur", "kurtar", "kurtul", "kurul", "ok", "ol", "oluş", "otur", "oyna", "öde", "öğren", "öldür", "öl", "paylaş", 
+    "sağla", "sağlan", "sahip ol", "san", "satın al", "sat", "say", "seç", "sev", "seyret", "sok", "sor", "söyle", 
+    "söylen", "söz et", "sun", "sunul", "sürdür", "sür", "tanı", "taşı", "tercih et", "topla", "toplan", "tut", 
+    "tutul", "uğraş", "ulaş", "unut", "uygula", "uygulan", "uy", "uyu", "uzan", "uzat", "üret", "var", "veril", 
+    "ver", "vur", "yakala", "yaklaş", "yak", "yan", "yapıl", "yap", "yaptır", "yararlan", "yarat", "yardımcı ol", 
+    "yaşa", "yat", "yayıl", "yayımlan", "yazıl", "yaz", "ye", "yet", "yüksel", "yürü"
+]
