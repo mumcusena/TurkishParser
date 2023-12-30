a1431199c96427a513d2fc9d5bde52fc8a067629
mumcusena <mumcusena.222@gmail.com>
2023-12-30 16:55:27 +0300

    noun lexicon added

---
+++ noun_lexicon.py
@@ -0,0 +1,14 @@
+noun_lexicon = ["kitap", "kalem", "silgi", "defter", "hediye", "roman", "hikaye", "öykü",
+                 "akşam", "yemek", "yardım", "arkadaş", "anne", "baba", "kardeş", "abla", 
+                 "hesap", "eğitim", "neden", "adam", "kadın", 'kız', 'oğlan', 'ders', 'okul',
+                 'dershane', 'hoca', 'öğretmen', 'öğrenci', 'abi', 'parmak', 'el', 'bilgisayar', 
+                 'ekran', 'televızyon', 'voleybol' , 'hafta', 'yıl', 'saniye', 'saat', 'dakika',
+                 'su', 'kahve', 'bira', 'buz', 'çay', 'koca', 'eş', 'erkek evlat', 'balık',
+                 'tavuk', 'dana', 'domuz', 'mutfak', 'banyo', 'yatak', 'kanepe', 'hemşire', 
+                 'otel', 'çocuk', 'doktor', 'bugün', 'yarın', 'dün', 'kafa', 'yağmur', 'kar',
+                 'bulut', 'ayakkabı', 'gömlek', 'çorap', 'pantolon', 'arkadaş', 'yetişkin',
+                 'güneş', 'ay']
+
+print(len(noun_lexicon))
+
+# https://www.turkishclass101.com/turkish-vocabulary-lists/50-most-common-nouns
