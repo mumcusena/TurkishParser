3d143317746d6bed0603897023a6b83c56382d93
mumcusena <mumcusena.222@gmail.com>
2023-12-30 16:07:03 +0300

    added new verbs

--- verb_lexicon.py
+++ verb_lexicon.py
@@ -1,18 +1,20 @@
 verb_lexicon = [
-    "aç", "ağla", "ak", "al", "anla", "anlat", "ara", "art", "aş", "atıl", "at", "ayır", "ayrıl", "azal", "bağır", 
-    "bağla", "bak", "bas", "başla", "bekle", "belirle", "belirlen", "belirt", "benz", "bırak", "bilin", "bil", 
-    "bin", "bitir", "bit", "bozul", "bul", "bulun", "büyü", "çalış", "çal", "çekil", "çek", "çevir", "çıkarıl", 
+    "aç", "affet", "ağla", "ak", "al", "alay", "anla", "anlaş", "anlat", "ara", "art", "aş", "atıl", "at", "ayır", "ayrıl", "azal", "bağır", 
+    "bağla", "bak", "bas", "bastır", "başla", "bat", "beğen", "bekle", "belirle", "belirlen", "belirt", "benz", "bırak", "bilin", "bil", 
+    "bin", "biriktir", "bitir", "bit", "bık", "bozul", "bul", "bulun", "büyü", "çalış", "çal", "çekil", "çek", "çevir", "çıkarıl", 
     "çıkar", "çık", "çiz", "dayan", "değerlendir", "değiş", "değiştir", "de", "dem", "devam et", "dikkat et", 
     "dile", "dinle", "doğ", "dolaş", "doldur", "dön", "dönüş", "dur", "duy", "düş", "düşün", "düzenle", "ekle", 
     "etkile", "et", "evlen", "fark et", "geçir", "geç", "geliş", "geliştir", "gel", "gerçekleş", "gere", "getir", 
     "gir", "git", "giy", "gönder", "gör", "görün", "görüş", "göster", "götür", "gül", "hareket et", "hatırla", 
     "hazırla", "hazırlan", "hisset", "iç", "ifade et", "ilerle", "ilgilen", "inan", "incele", "in", "istem", "isten", 
     "izle", "kabul et", "kaç", "kaldır", "kalk", "kal", "kapan", "kapat", "karış", "karıştır", "karşıla", "karşılaş", 
-    "katıl", "kaybet", "kazan", "kesil", "kes", "kıl", "konuş", "kork", "koru", "koş", "koy", "kullanıl", "kullan", 
-    "kur", "kurtar", "kurtul", "kurul", "ok", "ol", "oluş", "otur", "oyna", "öde", "öğren", "öldür", "öl", "paylaş", 
-    "sağla", "sağlan", "sahip ol", "san", "satın al", "sat", "say", "seç", "sev", "seyret", "sok", "sor", "söyle", 
+    "katıl", "kaybet", "kavuş", "kaynat", "kazan", "kesil", "kes", "keşfet", "kirala", "kırıl", "kıl", "konuş", "kork", "koru", "koş", "koy", "kop", "közle", "kullanıl", "kullan", 
+    "kur", "kurtar", "kurtul", "kurul", "kutla", "küs", "nişanlan", "numaralandır", "ok", "ol", "oluş", "oluştur", "otur", "oyna", "öde", "öğren", "öldür", "öl", "öner", "övün", "ör", "park", "paylaş", "pişir", "rastla", "rendele", 
+    "sağla", "sağlan", "sahip ol", "sakla", "saldır", "san", "satın al", "sat", "say", "seç", "sev", "seyret", "serp", "sok", "sor", "söyle", 
     "söylen", "söz et", "sun", "sunul", "sürdür", "sür", "tanı", "taşı", "tercih et", "topla", "toplan", "tut", 
     "tutul", "uğraş", "ulaş", "unut", "uygula", "uygulan", "uy", "uyu", "uzan", "uzat", "üret", "var", "veril", 
     "ver", "vur", "yakala", "yaklaş", "yak", "yan", "yapıl", "yap", "yaptır", "yararlan", "yarat", "yardımcı ol", 
     "yaşa", "yat", "yayıl", "yayımlan", "yazıl", "yaz", "ye", "yet", "yüksel", "yürü"
 ]
+
+print(len(verb_lexicon))
